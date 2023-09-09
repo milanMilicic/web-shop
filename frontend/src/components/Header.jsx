187c5b4ad659f6b7632330d83ffab90577f769b9
milanMilicic <milanm.milicic@gmail.com>
2023-09-09 16:39:11 +0200

    Number of items in cart

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,12 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png'
 import {LinkContainer} from 'react-router-bootstrap'
+import { useSelector } from 'react-redux';
 
 function Header() {
+    const {cartItems} = useSelector(state => state.cart);
+
   return (
     <header>
         <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
@@ -18,7 +21,16 @@ function Header() {
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='ms-auto'>
                         <LinkContainer to='/cart'>
-                            <Nav.Link><FaShoppingCart /> Cart</Nav.Link>
+                            <Nav.Link>
+                                <FaShoppingCart /> Cart
+                                {cartItems.length > 0 && (
+                                    <Badge pill bg='success' style={{marginLeft: '5px'}}>
+                                        {cartItems.reduce((sum, item) => {
+                                            return sum + item.qty;
+                                        }, 0)}
+                                    </Badge>
+                                )}
+                            </Nav.Link>
                         </LinkContainer>
                         <LinkContainer to='/login'>
                             <Nav.Link><FaUser /> Sign In</Nav.Link>

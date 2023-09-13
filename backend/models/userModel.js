8093b27bcf9dd29c769cfb6a7be9d4732d190bb8
milanMilicic <milanm.milicic@gmail.com>
2023-09-13 17:45:53 +0200

    Register user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,7 +27,17 @@ const userSchema = new mongoose.Schema({
 
 userSchema.methods.matchPassword = async function(enteredPassword){
     return await bcrypt.compare(enteredPassword, this.password);
-}
+};
+
+//this will be executed before new User is saved to the DB
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')){
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 

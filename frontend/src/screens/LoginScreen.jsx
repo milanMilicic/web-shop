a89ea0d0c249d13c79bd4369942df317edd226a9
milanMilicic <milanm.milicic@gmail.com>
2023-09-14 11:06:50 +0200

    Login Form

---
+++ frontend/src/screens/LoginScreen.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import FormContainer from '../components/FormContainer'
+
+function LoginScreen() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log('Submit');
+    }
+
+  return (
+    <FormContainer>
+        <h1>Sign In</h1>
+
+        <Form onSubmit={submitHandler}>
+            <Form.Group controlId='email' className='my-3'>
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
+            </Form.Group> 
+
+            <Form.Group controlId='password' className='my-3'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
+            </Form.Group> 
+
+            <Button type='submit' variant='primary' className='mt-2'>Sign In</Button>
+        </Form>
+
+        <Row className='py-3'>
+            <Col>
+                <Link to='/register'>Don't have an account?</Link>
+            </Col>
+        </Row>
+    </FormContainer>
+  )
+}
+export default LoginScreen

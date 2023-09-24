c43b1bba304d75cdcb33ea74f5a9551e46db4a6e
milanMilicic <milanm.milicic@gmail.com>
2023-09-24 18:15:35 +0200

    Other user methods

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -144,21 +144,42 @@ const updateUserProfile = asyncHandler( async (req, res) => {
 //Private/Admin
 // GET /api/users
 const getUsers = asyncHandler( async (req, res) => {
-    res.send('get users');
+    const users = await User.find({});
+    res.status(200).json(users);
 });
 
 
 //Private/Admin
 // GET /api/users/:id
 const getUserByID = asyncHandler( async (req, res) => {
-    res.send('get user by id');
+    const user = await User.findById(req.params.id).select('-password');
+
+    if(user){
+        res.status(200).json(user);
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 
 //Private/Admin
 // DELETE /api/users/:id
 const deleteUser = asyncHandler( async (req, res) => {
-    res.send('delete users');
+    const user = await User.findById(req.params.id);
+
+    if(user){
+        if(user.isAdmin){
+            res.status(400);
+            throw new Error('Cannot delete admin user');
+        }
+
+        await User.deleteOne({_id: user._id});
+        res.status(200).json({message: 'User deleted successfully'});
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 
@@ -166,7 +187,25 @@ const deleteUser = asyncHandler( async (req, res) => {
 //Private/Admin
 // PUT /api/users/:id
 const updateUser = asyncHandler( async (req, res) => {
-    res.send('update users');
+    const user = await User.findById(req.params.id);
+
+    if(user){
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        user.isAdmin = Boolean(req.body.isAdmin);
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        });
+    } else {
+        res.status(404);
+        throw new Errpr('User not found');
+    }
 });
 
 

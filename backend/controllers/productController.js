05c4aae7c1ce205dacc65645e8bb93fcd77dd40c
milanMilicic <milanm.milicic@gmail.com>
2023-09-26 09:49:18 +0200

    Create review - backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -74,4 +74,40 @@ const deleteProduct = asyncHandler( async (req, res) => {
     }
 });
 
-export { getProducts, getProductById, createProduct, updateProduct, deleteProduct }
+// Create a new review
+// POST /api/products/:id/review
+// Private
+const createReview = asyncHandler( async (req, res) => {
+    const {rating, comment} = req.body;
+    const product = await Product.findById(req.params.id);
+
+    if(product){
+        const alreadyReviewed = product.reviews.find((review) => review.user.toString() === req.user._id.toString());
+
+        if(alreadyReviewed){
+            res.status(400);
+            throw new Error('Product already reviewed');
+        }
+
+        const review = {
+            name: req.user.name,
+            rating: Number(rating),
+            comment,
+            user: req.user._id,
+        };
+
+        product.reviews.push(review);
+
+        product.numReviews = product.reviews.length;
+
+        product.rating = product.reviews.reduce((sum, review) => sum + review.rating, 0) / product.reviews.length;
+
+        await product.save();
+        res.status(201).json({message: 'Review added'});
+    } else {
+        res.status(404);
+        throw new Error('Resource not found');
+    }
+});
+
+export { getProducts, getProductById, createProduct, updateProduct, deleteProduct, createReview }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getProducts, getProductById, createProduct, updateProduct, deleteProduct } from '../controllers/productController.js';
+import { getProducts, getProductById, createProduct, updateProduct, deleteProduct, createReview } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
+router.route('/:id/reviews').post(protect, createReview);
 
 export default router;
